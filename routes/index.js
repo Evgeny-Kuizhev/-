e2dd90fddb00d61fee8d3b62696b509434a1cfcc
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-07-24 14:12:52 +0300

    developed 3 routes

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+router.get('/features', (req, res) => {
+    res.render('features');
+});
+
+router.get('/news', (req, res) => {
+    res.render('news');
+});
+
+
+module.exports = router;
