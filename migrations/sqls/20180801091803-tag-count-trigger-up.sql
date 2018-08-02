60ab7d33c29cd7b374340f3997d224e9f65f549d
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-02 10:41:11 +0300

    add trigger

--- migrations/sqls/20180801091803-tag-count-trigger-up.sql
+++ migrations/sqls/20180801091803-tag-count-trigger-up.sql
@@ -1,13 +1,22 @@
 -- изменяю таблицу записки и добавляю столбец
 -- кол-во тэгов со значением 0
 ALTER TABLE Note
-ADD tag_count INTEGER DEFAULT 0;
+ADD tags_count INTEGER DEFAULT 0;
 
--- создаю триггер
-CREATE TRIGGER incr_tag_count
+-- создаю триггер на увеличение тегов
+CREATE TRIGGER incr_tags_count
 AFTER INSERT ON Note_Tag
 FOR EACH ROW
 BEGIN
-  UPDATE Note SET tag_count=tag_count+1
+  UPDATE Note SET tags_count=tags_count+1
    WHERE id=NEW.note_id;
+END;
+
+-- создаю триггер на уменьшение тегов
+CREATE TRIGGER decr_tags_count
+AFTER DELETE ON Note_Tag
+FOR EACH ROW
+BEGIN
+  UPDATE Note SET tags_count=tags_count-1
+   WHERE id=OLD.note_id;
 END;
