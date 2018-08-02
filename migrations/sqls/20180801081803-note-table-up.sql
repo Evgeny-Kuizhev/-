005b0b368aca5a872cc61c7b1cbc772d15b74aeb
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-02 13:47:41 +0300

    cxz

--- migrations/sqls/20180801081803-note-table-up.sql
+++ migrations/sqls/20180801081803-note-table-up.sql
@@ -3,7 +3,7 @@ CREATE TABLE "Note" (
   user_id   INTEGER     NOT NULL,
   title     TEXT        NOT NULL,
   UNIQUE (user_id, title),
-  
+
   CONSTRAINT Note_fk_user_id FOREIGN KEY (user_id)
     REFERENCES "User" (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
