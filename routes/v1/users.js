72b3cd0aaae580a54c05d3668152b9d04664760a
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-03 16:28:15 +0300

    created

---
+++ routes/v1/users.js
@@ -0,0 +1,13 @@
+const
+    express = require('express'),
+    router = express.Router(),
+    usersCntr = require('../../controllers/users');
+
+
+router.get('/', usersCntr.getAll);
+
+router.get('/:id', usersCntr.getOne);
+
+
+
+module.exports = router;
