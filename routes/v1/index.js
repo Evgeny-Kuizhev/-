26881b595b3fbe299c5e6b7eb6d8b89cc1ba0846
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-03 16:27:56 +0300

    for controllers

---
+++ routes/v1/index.js
@@ -0,0 +1,16 @@
+const
+    express = require('express'),
+    router = express.Router(),
+    usersRoute = require('./users'),
+    notesRoute = require('./notes'),
+    tagsRoute = require('./tags');
+
+
+router.get('/', (req, res) => res.send('API V1'));
+
+router.use('/users', usersRoute);
+router.use('/notes', notesRoute);
+router.use('/tags', tagsRoute);
+
+
+module.exports = router;
