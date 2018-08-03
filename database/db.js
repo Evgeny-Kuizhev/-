d6dbdc7776c614b8b4d5d2eac810f0f20a4d7a48
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-03 16:25:55 +0300

    перемещен в папку database

---
+++ database/db.js
@@ -0,0 +1,16 @@
+const
+    sqlite3 = require('sqlite3').verbose(),
+    db = new  sqlite3.Database('database/db.sqlite', () =>  db.run('PRAGMA foreign_keys=on') ),
+    seed = require('./seed');
+
+db.serialize(() => {
+    db.get('PRAGMA foreign_keys', (err, res) => {
+        if (err) throw err;
+        console.log('Default PRAGMA switched to ' + res.foreign_keys);
+    });
+
+    // use seed?
+    // db.serialize( () => seed(db) );
+});
+
+module.exports = db;
