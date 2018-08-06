dd17559cd2bef64d340b0968860034bf45a0b879
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-06 17:45:33 +0300

    add head

--- api/controllers/helpfullRespond.js
+++ api/controllers/helpfullRespond.js
@@ -1,12 +1,23 @@
-function _respond(res, success, message, code) {
-    message.success = success;
-    res.status(code).json(message);
+'use strict'
+
+const headers = {
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10,
+    "Content-Type": "application/json"
+  };
+
+function _respond(res, success, body, status) {
+    body.success = success;
+    res.set(headers);
+    res.status(status).send(body);
 }
 
-exports.success = (res, message) => {
-    _respond(res, true, message, 200);
+exports.success = (res, data) => {
+    _respond(res, true, data, 200);
 }
 
-exports.failure = (res, message, code) => {
-    _respond(res, false, message, code);
+exports.failure = (res, data, code) => {
+    _respond(res, false, data, code);
 }
