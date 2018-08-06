39e7042d3ba463ce90add13feb72e663e7d01f72
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-06 17:22:02 +0300

    moved

---
+++ api/controllers/users.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const
+    respond = require('./helpfullRespond'),
+    users = require('../models/Users');
+
+console.log(users.getAll);
+exports.getAll = (req, res) => {
+    function cb(err, users) {
+        if (err) respond.failure(res, {message: 'Ошибка бд.'}, 500);
+        //if (!users.length) respond.failure(res, {message: 'Пользователи не найдены!'}, 404);
+        respond.success(res, {users, message: 'Пользователи полученны!'});
+    }
+    users.getAll(cb);
+}
+
+exports.getOne = (req, res) => {
+    if (!req.params && !req.params.id){
+        respond.failure(res, {message: 'Плохой запрос'}, 400);
+    }
+    function cb(err, user) {
+        if (err) respond.failure(res, {message: 'Ошибка бд.'}, 500);
+        //if (!user.length) respond.failure(res, {message: 'Пользователь не найден!'}, 404);
+        console.log(user);
+        respond.success(res, {user, message: 'Пользователи полученны!'});
+    }
+    users.getOne(req.params.id, cb);
+}
