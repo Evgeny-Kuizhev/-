f36b0b6990f4e200e9b54961da09e4ea2b4f713c
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-06 09:59:50 +0300

    refactoring

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,7 @@ const express = require('express'),
 
 
 router.get('/', (req, res) => res.render('home') );
-
 router.get('/features', (req, res) => res.render('features') );
-
 router.get('/news', (req, res) => res.render('news') );
 
 router.use('/api/v1', apiV1)
