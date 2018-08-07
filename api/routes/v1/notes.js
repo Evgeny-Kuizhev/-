4d0690c638bb68d63b7800e762c133790ea06e82
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-07 14:41:48 +0300

    Разработанно получение тегов по зиписки и получение записок по тегу

--- api/routes/v1/notes.js
+++ api/routes/v1/notes.js
@@ -2,10 +2,15 @@
 
 const
     express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    notesCntr = require('../../controllers/notes');
 
-
-
+// получить все записки
+router.get('/', notesCntr.getAll);
+// получить конкретную записку
+router.get('/:id', notesCntr.getOne);
+// получить записки по тегу
+router.get('/tag/:id', notesCntr.getTagNotes);
 
 
 
--- api/routes/v1/users.js
+++ api/routes/v1/users.js
@@ -5,11 +5,12 @@ const
     router = express.Router(),
     usersCntr = require('../../controllers/users');
 
-
+// получить всех пользователей
 router.get('/', usersCntr.getAll);
-
+// получить конкретного пользователя
 router.get('/:id', usersCntr.getOne);
-
+// получить записки пользователя
+router.get('/:id/notes', usersCntr.getUserNotes);
 
 
 module.exports = router;
