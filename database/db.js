9ef0b321b0a88e468a8f5e5f66f04d9c0d8d9141
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-08 14:35:22 +0300

    only connect to db without reset

--- database/db.js
+++ database/db.js
@@ -3,18 +3,26 @@
 const
     sqlite3 = require('sqlite3').verbose(),
     Promise  = require('bluebird'),
-    db = Promise.promisifyAll( new  sqlite3.Database('./db.sqlite', () =>  db.run('PRAGMA foreign_keys=on') ) ),
     seed = require('./seed');
+    const db = Promise.promisifyAll( new  sqlite3.Database(__dirname+'/db.sqlite'));
 
-db.serialize(() => {
+db.serialize( async () => {
+    const isResetDB = 1;
+    if (isResetDB) {
+        const
+            DBMigrate = require('db-migrate'),
+            dbmigrate = Promise.promisifyAll( DBMigrate.getInstance(true, {cwd: __dirname}) );
+        await dbmigrate.resetAsync();
+        await dbmigrate.upAsync();
+    }
+
+    db.run('PRAGMA foreign_keys=on');
+
     db.get('PRAGMA foreign_keys', (err, res) => {
         if (err) throw err;
         console.log('Default PRAGMA switched to ' + res.foreign_keys);
     });
-
-    // use seed?
     db.serialize( () => seed(db) );
 });
-
 
 module.exports = db;
