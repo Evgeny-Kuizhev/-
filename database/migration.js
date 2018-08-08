5bfcc8982603135cabb0fd84312de41b1e0e62d0
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-08 14:34:18 +0300

    useless

--- database/migration.js
+++
@@ -1,9 +0,0 @@
-const
-    Promise  = require('bluebird'),
-
-    DBMigrate = require('db-migrate'),
-    dbmigrate = DBMigrate.getInstance(true, {cwd: __dirname});
-
-dbmigrate.reset( () => {
-    dbmigrate.up();
-})
