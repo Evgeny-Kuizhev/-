66d7b393b07f1f8d635ad4eb499d61d30cc7c53d
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-09 11:18:55 +0300

    add route

--- api/routes/v1/notes.js
+++ api/routes/v1/notes.js
@@ -10,7 +10,9 @@ router.get('/', notesCntr.getAll);
 // получить конкретную записку
 router.get('/:id', notesCntr.getOne);
 // получить записки по тегу
-router.get('/tag/:id', notesCntr.getTagNotes);
+router.get('/tags/:id', notesCntr.getTagNotes);
+// создать записку
+router.post('/users/:id')
 
 
 
