3c73bc29d38eb5e4e8acb56adc3dd9638b902a27
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-10 11:02:28 +0300

    prepering for delete route

--- api/routes/v1.js
+++ api/routes/v1.js
@@ -10,23 +10,26 @@ const
 // ---------------USER ROUTES--------------- //
 router.get('/users', userCntr.getAll);
 router.get('/user/:id', userCntr.getOne);
-router.get('/user/:id/notes', userCntr.getUserNotes);
+router.get('/user/:id/notes', userCntr.getNotes);
 router.post('/user', userCntr.create);
 router.put('/user/:id', userCntr.change);
+router.delete('/user/:id', userCntr.delete);
 
 
 // ---------------NOTES ROUTES--------------- //
 router.get('/notes', noteCntr.getAll);
 router.get('/note/:id', noteCntr.getOne);
-router.get('/notes/tag/:id', noteCntr.getTagNotes);
+router.get('/note/:id/tags', noteCntr.getTags);
 router.post('/note', noteCntr.create);
 router.put('/note/:id', noteCntr.change);
+router.delete('/note/:id', noteCntr.delete)
 
 
 // ---------------TAGS ROUTES--------------- //
 router.get('/tags', tagCntr.getAll);
-router.get('/tags/note/:id', tagCntr.getNoteTags);
+router.get('/tag/:id/notes', tagCntr.getNotes);
 router.post('/tag', tagCntr.create);
+router.delete('/tag/:id', tagCntr.delete)
 
 
 module.exports = router;
