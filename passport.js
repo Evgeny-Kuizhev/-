2c4c31a231d3310741b150878311e5ee59e58bab
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-15 16:45:52 +0300

    mb

---
+++ passport.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const passport = require('passport')
+const bcrypt = require('bcrypt')
+const LocalStrategy = require('passport-local').Strategy
+
+const authenticationMiddleware = require('./middleware')
+
+// Generate Password
+const saltRounds = 10
+const myPlaintextPassword = 'my-password'
+const salt = bcrypt.genSaltSync(saltRounds)
+const passwordHash = bcrypt.hashSync(myPlaintextPassword, salt)
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (!user.verifyPassword(password)) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
