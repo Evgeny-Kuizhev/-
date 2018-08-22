023030c5f77d21fa11900054477a589aced81e1a
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-08-22 12:55:05 +0300

    login

--- app/authenticate/middleware.js
+++ app/authenticate/middleware.js
@@ -4,7 +4,7 @@ function authenticationMiddleware(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
-    res.redirect('/login');
+    res.redirect('login');
 }
 
   module.exports = authenticationMiddleware;
